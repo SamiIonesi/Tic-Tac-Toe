bb2b275f2b4dcee17f6c86d0fd456aff87f0060b
samiionesi <ionesi.samuel@gmail.com>
2024-08-13 14:10:03 +0300

    TicTacToe using OOP
    Updating the TicTacToe game from procedural to object-oriented method

--- Tic_Tac_toe.cpp
+++ Tic_Tac_toe.cpp
@@ -1,126 +1,158 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
-void printArr(char *arr, int length)
+
+class TicTacToe
 {
-    for(int i = 0; i < length; i++)
+private:
+    int length;
+    char gameTable[];
+
+public:
+
+    int getLength() { return this->length; }
+    char* getGameTable() {return this->gameTable; }
+
+    void fillGameTable(char *arr, int length)
     {
-        if(i == 3 || i == 6)
-            cout << endl;
-        cout << arr[i] << "  ";
+        for(int i = 0; i < length; i++)
+        {
+            arr[i] = 48 + i;
+        }
     }
 
-    cout << endl;
-}
-
-bool check(char *arr, int lenght, char symbol)
-{
-    if(arr[0] == symbol && arr[1] == symbol && arr[2] == symbol)
-        return true;
-    if(arr[3] == symbol && arr[4] == symbol && arr[5] == symbol)
-        return true;
-    if(arr[6] == symbol && arr[7] == symbol && arr[8] == symbol)
-        return true;
-    if(arr[0] == symbol && arr[3] == symbol && arr[6] == symbol)
-        return true;
-    if(arr[1] == symbol && arr[4] == symbol && arr[7] == symbol)
-        return true;
-    if(arr[2] == symbol && arr[5] == symbol && arr[8] == symbol)
-        return true;
-    if(arr[0] == symbol && arr[4] == symbol && arr[8] == symbol)
-        return true;
-    if(arr[2] == symbol && arr[4] == symbol && arr[6] == symbol)
-        return true;
-
-    return false;
-}
-
-bool turn(char *arr, int length, bool checkForWinner, int player, int playerNumber, char symbol)
-{
-    cout << endl << "Player " << playerNumber << ": " << endl;
-    cin >> player;
-
-    while(arr[player] == 'O' || arr[player] == 'X')
+    TicTacToe()
     {
-        cout << endl << "Box " << player << " is already fill. Try again: " << endl;
+        this->length = 9;
+        fillGameTable(gameTable, length);
+    }
+
+    void printGameTable(char *arr, int length)
+    {
+        cout << endl;
+
+        for(int i = 0; i < length; i++)
+        {
+            if(i == 3 || i == 6)
+                cout << endl;
+            cout << arr[i] << "  ";
+        }
+
+        cout << endl;
+    }
+
+    bool check(char *arr, int lenght, char symbol)
+    {
+        if(arr[0] == symbol && arr[1] == symbol && arr[2] == symbol)
+            return true;
+        if(arr[3] == symbol && arr[4] == symbol && arr[5] == symbol)
+            return true;
+        if(arr[6] == symbol && arr[7] == symbol && arr[8] == symbol)
+            return true;
+        if(arr[0] == symbol && arr[3] == symbol && arr[6] == symbol)
+            return true;
+        if(arr[1] == symbol && arr[4] == symbol && arr[7] == symbol)
+            return true;
+        if(arr[2] == symbol && arr[5] == symbol && arr[8] == symbol)
+            return true;
+        if(arr[0] == symbol && arr[4] == symbol && arr[8] == symbol)
+            return true;
+        if(arr[2] == symbol && arr[4] == symbol && arr[6] == symbol)
+            return true;
+
+        return false;
+    }
+
+    bool turn(char *arr, int length, bool checkForWinner, int player, int playerNumber, char symbol)
+    {
+        cout << endl << "Player " << playerNumber << ": " << endl;
         cin >> player;
-    }
 
-    arr[player] = symbol;
-    checkForWinner = check(arr, length, symbol);
-
-    printArr(arr, length);
-
-    if(checkForWinner)
-    {
-        cout << "Player " << playerNumber << " won the game." << endl;
-        return true;
-    }
-    return false;
-}
-
-void game(char *arr, int length)
-{
-    int player1;
-    int player2;
-    bool checkForWinner = false;
-    bool playAgain = true;
-    int play;
-
-    while(playAgain)
-    {
-        printArr(arr, length);
-
-        while(!checkForWinner)
+        while(arr[player] == 'O' || arr[player] == 'X')
         {
-            checkForWinner = turn(arr, length, checkForWinner, player1, 1, 'X');
-
-            if(checkForWinner)
-                break;
-
-            checkForWinner = turn(arr, length, checkForWinner, player2, 2, 'O');
+            cout << endl << "Box " << player << " is already fill. Try again: " << endl;
+            cin >> player;
         }
 
-        cout << endl << "Do you want to play again? " << endl;
-        cout << "Type 1 for 'yes' or 2 for 'no'" << endl;
-        cin >> play;
+        arr[player] = symbol;
+        checkForWinner = check(arr, length, symbol);
 
-        if(play == 2)
+        printGameTable(arr, length);
+
+        if(checkForWinner)
         {
-            playAgain = false;
-            cout << "Game over. Goodbye!" << endl;
+            cout << "Player " << playerNumber << " won the game." << endl;
+            return true;
         }
-        else if(play == 1)
-        {
-            checkForWinner = false;
+        return false;
+    }
 
-            for(int i = 0; i < length; i++)
+    void game(char *arr, int length)
+    {
+        int player1;
+        int player2;
+        bool checkForWinner = false;
+        bool playAgain = true;
+        int play;
+
+        while(playAgain)
+        {
+            cout << "Welcome to the Tic-Tac-Toe game!" << endl;
+            printGameTable(arr, length);
+
+            while(!checkForWinner)
             {
-                arr[i] = 48 + i;
+                checkForWinner = turn(arr, length, checkForWinner, player1, 1, 'X');
+
+                if(checkForWinner)
+                    break;
+
+                checkForWinner = turn(arr, length, checkForWinner, player2, 2, 'O');
+            }
+
+            cout << endl << "Do you want to play again? " << endl;
+            cout << "Type 1 for 'yes' or 2 for 'no'" << endl;
+            cin >> play;
+
+            if(play == 2)
+            {
+                playAgain = false;
+                cout << "Game over. Goodbye!" << endl;
+            }
+            else if(play == 1)
+            {
+                checkForWinner = false;
+
+                fillGameTable(arr, length);
+            }
+            else
+            {
+                //this one is not working properly
+                while(play != 1 && play != 2)
+                {
+                    cout << "Incorrect number. Try again: " << endl;
+
+                    cin >> play;
+
+                    cin.clear();
+
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                }
             }
         }
-        else
-        {
-            cout << "Incorrect number. try again: " << endl;
-            cin >> play;
-        }
     }
-}
+
+};
+
 
 int main()
 {
 
-    char arr[9] = {'0', '1', '2', '3', '4', '5', '6', '7', '8'};
-    int length = sizeof(arr) / sizeof(arr[0]);
+    TicTacToe ttt;
 
-    bool chechForWinner;
-
-    cout << "Welcome to the Tic-Tac-Toe game!" << endl;
-
-    //printArr(arr, length);
-
-    game(arr, length);
+    ttt.game(ttt.getGameTable(), ttt.getLength());
 
     return 0;
 }
